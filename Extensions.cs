6964eec091b690668e6aaa525008eb2a26998d43
steveoh <sgourley@utah.gov>
2020-01-28 14:07:03 -0700

    feat: format time and add colors

---
+++ Extensions.cs
@@ -0,0 +1,37 @@
+using System.Drawing;
+using Pastel;
+
+namespace cambiador.Extensions {
+  public static class Time {
+    public static string FriendlyFormat(this long milliseconds) {
+      const double minute = 60;
+      const double hour = 60D * minute;
+      var seconds = milliseconds / 1000D;
+
+      if (seconds < 10) {
+        return $"{milliseconds} ms";
+      }
+
+      if (seconds < 90) {
+        return $"{seconds} seconds";
+      }
+
+      if (seconds < 90 * minute) {
+        return $"{seconds / minute} minutes";
+      }
+
+      return $"{seconds / hour} hours";
+    }
+  }
+
+  public static class Colors {
+    public static string AsMagenta(this string text) => text.Pastel(Color.FromArgb(201, 149, 223));
+    public static string AsBlue(this string text) => text.Pastel(Color.FromArgb(129, 189, 237));
+    public static string AsCyan(this string text) => text.Pastel(Color.FromArgb(123, 192, 203));
+    public static string AsYellow(this string text) => text.Pastel(Color.FromArgb(213, 172, 128));
+    public static string AsBlack(this string text) => text.Pastel(Color.Black);
+    public static string AsRed(this string text) => text.Pastel(Color.FromArgb(220, 136, 138));
+    public static string AsRedBg(this string text) => text.PastelBg(Color.FromArgb(220, 136, 138));
+    public static string AsWhiteBg(this string text) => text.PastelBg(Color.FromArgb(186, 191, 201));
+  }
+}
