90fb40c785debbaec73b38ed07a918790670e96f
steveoh <sgourley@utah.gov>
2020-01-27 19:30:16 -0700

    wip: single table hash

--- Program.cs
+++ Program.cs
@@ -11,71 +11,35 @@ using System.Threading.Tasks;
 
 namespace cambiador {
   internal class Program {
-    private static async Task Main() {
-      Console.WriteLine("Connecting to the databases");
-      using var source_connection = Databases.SourceDatabase.GetConnection();
-      using var hash_connection = Databases.HashDatabase.GetConnection();
-      var timer = new Stopwatch();
+    static string changeTable = "CHANGEDETECTION";
+    static string updateHashSql = "UPDATE ChangeDetection SET last_modified=GETDATE(), [hash]=@hash WHERE LOWER(table_name)=LOWER(@tableName)";
+    static string getHashSql = $"SELECT [hash] FROM {changeTable} WHERE LOWER(table_name)=LOWER(@tableName)";
 
-      if (!await EnsureChangeDetectinTableExists(source_connection)) {
-        Console.WriteLine("ChangeDetection table does not exist. Please create it");
+    private static async Task Main() {
+      Console.WriteLine("Connecting to the source database");
+      using var connection = Databases.SourceDatabase.GetConnection();
+
+      if (!await EnsureChangeDetectionTableExists(connection)) {
+        Console.WriteLine($"{changeTable} table does not exist. Please create it");
 
         return;
       }
 
       Console.WriteLine("Discovering tables and schemas");
-      var tableFieldMap = await DiscoverAndGroupTablesWithFields(source_connection);
+      var tableFieldMap = await DiscoverAndGroupTablesWithFields(connection);
 
       foreach (var table in tableFieldMap) {
         var tableName = table.Key;
         var fields = table.Value;
 
-        // create the database table if it doesn't exist yet
-        Console.WriteLine($"Ensuring {tableName} exists");
-        var tableOfHashes = await EnsureHashTableExists(tableName, hash_connection);
-
-        var hashesAsOfLastRun = new HashSet<string>(await source_connection.QueryAsync<string>($"SELECT [hash] from {tableOfHashes}"));
-        var hashesAsOfNow = await CreateHashesFromTable(tableName, fields, source_connection);
+        var hashAsOfLastRun = await connection.QueryFirstAsync<string>(getHashSql, new { tableName });
+        var hashAsOfNow = await CreateHashFromTableRows(tableName, fields, connection);
 
         // have we hashed this dataset already?
-        if (hashesAsOfLastRun.Count < 1) {
-          Console.WriteLine($"Assuming first run for {tableOfHashes}, inserting hashes");
+        if (string.IsNullOrEmpty(hashAsOfLastRun) || hashAsOfLastRun != hashAsOfNow) {
+          Console.WriteLine($"Changes detected. Updating the hash for {tableName}");
 
-          var inserts = hashesAsOfNow.Select(x => new Insert() { Hash = x });
-
-          timer = Stopwatch.StartNew();
-          await BulkInsert(tableOfHashes, hashesAsOfNow, hash_connection);
-          Console.WriteLine($"Insert: {timer.ElapsedMilliseconds}");
-
-          await source_connection.ExecuteAsync($"UPDATE ChangeDetection SET last_modified=GETDATE() WHERE table_name=@tableName", new { tableName });
-
-          continue;
-        }
-
-        Console.WriteLine("Finding differences");
-        timer = Stopwatch.StartNew();
-        hashesAsOfLastRun.SymmetricExceptWith(hashesAsOfNow);
-        Console.WriteLine($"SymmetricExceptWith: {timer.ElapsedMilliseconds}");
-        timer.Stop();
-
-        var numberOfDifferences = hashesAsOfLastRun.Count;
-        if (numberOfDifferences > 0) {
-          Console.WriteLine($"{numberOfDifferences} differences detected");
-          timer = Stopwatch.StartNew();
-
-          await hash_connection.ExecuteAsync($"TRUNCATE TABLE {tableOfHashes}");
-
-          Console.WriteLine($"Truncate: {timer.ElapsedMilliseconds}");
-
-          timer = Stopwatch.StartNew();
-
-          await BulkInsert(tableOfHashes, hashesAsOfNow, hash_connection);
-
-          Console.WriteLine($"Insert: {timer.ElapsedMilliseconds}");
-
-          timer.Stop();
-
-          await source_connection.ExecuteAsync($"UPDATE ChangeDetection SET last_modified=GETDATE() WHERE table_name=@tableName", new { tableName });
+          await connection.ExecuteAsync(updateHashSql, new { hash = hashAsOfNow, tableName });
 
           continue;
         }
@@ -85,9 +49,7 @@ namespace cambiador {
     }
 
     // Creates the table on he first run or returns the name of the table if it already exists
-    private static async Task<bool> EnsureChangeDetectinTableExists(SqlConnection connection) {
-      const string changeTable = "CHANGEDETECTION";
-
+    private static async Task<bool> EnsureChangeDetectionTableExists(SqlConnection connection) {
       return await connection.QueryFirstOrDefaultAsync<bool>($"SELECT 1 FROM INFORMATION_SCHEMA.TABLES WHERE UPPER(TABLE_NAME)=@changeTable", new { changeTable });
     }
 
@@ -127,31 +89,18 @@ namespace cambiador {
       return tableFieldMap;
     }
 
-    private static async Task<string> EnsureHashTableExists(string table, SqlConnection connection) {
-      var tableHashes = $"{table.ToUpperInvariant()}_HASH";
-      var tableExists = await connection.QueryFirstOrDefaultAsync<bool>($"SELECT 1 FROM INFORMATION_SCHEMA.TABLES WHERE UPPER(TABLE_NAME)=@tableHashes", new { tableHashes });
-
-      if (tableExists) {
-        return tableHashes;
-      }
-
-      var createTableSql = $"CREATE TABLE [{tableHashes}]([id] [int] IDENTITY(1,1) NOT NULL, [hash] [nvarchar](50) NOT NULL)";
-
-      await connection.ExecuteAsync(createTableSql);
-
-      return tableHashes;
-    }
-
-    private static async Task<HashSet<string>> CreateHashesFromTable(string table, IEnumerable<string> fields, SqlConnection connection) {
+    private static async Task<string> CreateHashFromTableRows(string table, IEnumerable<string> fields, SqlConnection connection) {
       // get all of the data from the table
       Console.WriteLine($"Querying {table} for all data");
       var timer = Stopwatch.StartNew();
-      var rows = await connection.QueryAsync($"SELECT {string.Join(',', fields)} FROM {table}");
+
+      var rows = await connection.QueryAsync($"SELECT {string.Join(',', fields)} FROM {table} ORDER BY OBJECTID");
+
       Console.WriteLine($"Query completed: {timer.ElapsedMilliseconds}");
       timer.Stop();
 
       var numberOfRecords = ((List<dynamic>)rows).Count;
-      var hashesAsOfNow = new HashSet<string>(numberOfRecords);
+      var hashesAsOfNow = new StringBuilder();
 
       Console.WriteLine($"Hashing {numberOfRecords} records");
       timer = Stopwatch.StartNew();
@@ -170,41 +119,17 @@ namespace cambiador {
           hashMe.Add(Convert.ToString(row[field]));
         }
 
-        var result = XXHash.Hash64(Encoding.UTF8.GetBytes(string.Join(string.Empty, hashMe))).ToString();
+        var hash = XXHash.Hash64(Encoding.UTF8.GetBytes(string.Join(string.Empty, hashMe))).ToString();
 
-        hashesAsOfNow.Add(result);
+        hashesAsOfNow.Append(hash);
       }
+
+      var result = XXHash.Hash64(Encoding.UTF8.GetBytes(hashesAsOfNow.ToString()));
 
       Console.WriteLine($"Hashing completed: {timer.ElapsedMilliseconds}");
       timer.Stop();
 
-      return hashesAsOfNow;
-    }
-
-    public static async Task BulkInsert(string table, HashSet<string> hashes, SqlConnection connection) {
-      var statements = GenerateBatchInsertStatements(table, hashes);
-
-      foreach (var statement in statements) {
-        await connection.ExecuteAsync(statement);
-      }
-    }
-
-    private static IList<string> GenerateBatchInsertStatements(string table, IReadOnlyCollection<string> values) {
-      var insertSql = $"INSERT INTO [{table}] ([hash]) VALUES ";
-      var batchSize = 1000;
-
-      var sql = new List<string>(values.Count);
-      var numberOfBatches = (int)Math.Ceiling((double)values.Count / batchSize);
-
-      for (var i = 0; i < numberOfBatches; i++) {
-        var batch = values.Skip(i * batchSize).Take(batchSize);
-
-        var valuesToInsert = batch.Select(x => $"('{x}')");
-
-        sql.Add(insertSql + string.Join(',', valuesToInsert));
-      }
-
-      return sql;
+      return Convert.ToString(result);
     }
   }
 }
