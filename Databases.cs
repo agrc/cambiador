248d6d92256b4f5e41f0462a6ab0cd090cbecab8
steveoh <sgourley@utah.gov>
2020-01-27 19:29:58 -0700

    chore: remove hash database

--- Databases.cs
+++ Databases.cs
@@ -5,8 +5,4 @@ namespace cambiador.Databases {
   internal static class SourceDatabase {
     public static SqlConnection GetConnection() => new SqlConnection(ConfigurationManager.ConnectionStrings["source"].ConnectionString);
   }
-
-  internal static class HashDatabase {
-    public static SqlConnection GetConnection() => new SqlConnection(ConfigurationManager.ConnectionStrings["hash"].ConnectionString);
-  }
 }
