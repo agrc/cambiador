a09209294a65ae52c97ec841bf1bfefd0f700807
steveoh <sgourley@utah.gov>
2024-04-11 16:38:36 -0700

    refactor: update namespaces

--- src/Models.cs
+++ src/Models.cs
@@ -1,27 +1,26 @@
-namespace cambiador {
-  public class FieldMetadata {
-    public string Db { get; set; } = default!;
-    public string Table { get; set; } = default!;
-    public string Field { get; set; } = default!;
-    public string FieldType { get; set; } = default!;
-    public string? Schema { get; set; }
-    public string TableName() {
-      if (string.IsNullOrEmpty(Schema)) {
-        return Table;
-      }
-
-      return $"{Db}.{Schema}.{Table}";
+namespace cambiador;
+public class FieldMetadata {
+  public string Db { get; set; } = default!;
+  public string Table { get; set; } = default!;
+  public string Field { get; set; } = default!;
+  public string FieldType { get; set; } = default!;
+  public string? Schema { get; set; }
+  public string TableName() {
+    if (string.IsNullOrEmpty(Schema)) {
+      return Table;
     }
-  }
 
-  public class Stats {
-    public int TotalRows { get; set; }
-    public ICollection<string> Changed { get; set; } = new List<string>(10);
-    public long QueryTime { get; set; } = 0;
-    public long HashTime { get; set; } = 0;
+    return $"{Db}.{Schema}.{Table}";
   }
+}
 
-  public class SendGridSettings {
-    public string? ApiKey { get; set; }
-  }
+public class Stats {
+  public int TotalRows { get; set; }
+  public ICollection<string> Changed { get; set; } = new List<string>(10);
+  public long QueryTime { get; set; } = 0;
+  public long HashTime { get; set; } = 0;
+}
+
+public class SendGridSettings {
+  public string? ApiKey { get; set; }
 }
